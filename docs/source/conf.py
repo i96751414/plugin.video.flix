7ada7fbf2bb980273b87534f33e58a5a7a3be119
i96751414 <i96751414@gmail.com>
2024-12-28 23:16:37 +0000

    Fix sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,36 +13,36 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../lib/api'))
+sys.path.insert(0, os.path.abspath("../../lib/api"))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Flix'
-copyright = '2020, i96751414'
-author = 'i96751414'
+project = "Flix"
+copyright = "2020, i96751414"
+author = "i96751414"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.11'
+release = "0.0.18"
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
 ]
 
-autodoc_member_order = 'bysource'
-autodoc_default_flags = ['members', 'show-inheritance']
+autodoc_member_order = "bysource"
+autodoc_default_flags = ["members", "show-inheritance"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -54,26 +54,26 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'github_button': True,
-    'github_type': 'star&v=2',
-    'github_user': 'i96751414',
-    'github_repo': 'plugin.video.flix',
-    'github_banner': False,
-    'font_family': 'Georgia',
+    "github_button": True,
+    "github_type": "star&v=2",
+    "github_user": "i96751414",
+    "github_repo": "plugin.video.flix",
+    "github_banner": False,
+    "font_family": "Georgia",
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 intersphinx_mapping = {
-    'https://docs.python.org/3.7': None,
-    'https://romanvm.github.io/Kodistubs': None,
+    "python": ("https://docs.python.org/3.7", None),
+    "Kodistubs": ("https://romanvm.github.io/Kodistubs", None),
 }
